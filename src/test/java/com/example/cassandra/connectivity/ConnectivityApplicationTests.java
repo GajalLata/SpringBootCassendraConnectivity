0a8e6724b2f4faa03acdddfc50b28824cc5eaefa
GajalLata <gajalsharma212@gmail.com>
2024-05-09 09:35:05 -0400

    initial commit

---
+++ src/test/java/com/example/cassandra/connectivity/ConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cassandra.connectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
